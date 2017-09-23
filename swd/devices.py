0430193821e3107904cc5b0494c932bd00294e69
enjedi <enjedi01@gmail.com>
2017-09-23 09:11:06 +0200

    Use Inheritance for devices

--- swd/devices.py
+++ swd/devices.py
@@ -6,25 +6,39 @@ Each class is made up of relative information for a USB device and organized int
 by device name, i.e. the class name.
 """
 
-class STLinkV2():
+class Device(object):
+    def __init__(self, description):
+        self._description   = description
+        self.ID_VENDOR      = None
+        self.ID_PRODUCT     = None
+        self.PIPE_OUT       = None
+        self.PIPE_IN        = None
+
+    def __str__(self):
+        return self._description
+
+
+class STLinkV2(Device):
     """ST-Link/V2 USB Device"""
-    ID_VENDOR   = 0x0483
-    ID_PRODUCT  = 0x3748
-    PIPE_OUT    = 0x02
-    PIPE_IN     = 0x81
-    VERSION     = "V2"
+    def __init__(self, description):
+        super().__init__(description)
+        self.ID_VENDOR   = 0x0483
+        self.ID_PRODUCT  = 0x3748
+        self.PIPE_OUT    = 0x02
+        self.PIPE_IN     = 0x81
 
 
-class STLinkV21():
+class STLinkV21(Device):
     """ST-Link/V2-1 USB Device"""
-    ID_VENDOR   = 0x0483
-    ID_PRODUCT  = 0x374b
-    PIPE_OUT    = 0x01
-    PIPE_IN     = 0x81
-    VERSION     = "V2-1"
+    def __init__(self, description):
+        super().__init__(description)
+        self.ID_VENDOR   = 0x0483
+        self.ID_PRODUCT  = 0x374b
+        self.PIPE_OUT    = 0x01
+        self.PIPE_IN     = 0x81
 
 
 DEVICE_LIST = {
-    STLinkV2.__name__   : STLinkV2(),
-    STLinkV21.__name__  : STLinkV21()
+    STLinkV2.__name__   : STLinkV2("STLink V2"),
+    STLinkV21.__name__  : STLinkV21("STLink V2-1")
 }
