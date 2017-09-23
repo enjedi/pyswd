81bf6f8ffa43dce9baba33abd947c7e444338091
enjedi <enjedi01@gmail.com>
2017-09-23 09:48:40 +0200

    Move STLink enumeration codes into device subclasses

--- swd/devices.py
+++ swd/devices.py
@@ -26,6 +26,104 @@ class STLinkV2(Device):
         self.ID_PRODUCT  = 0x3748
         self.PIPE_OUT    = 0x02
         self.PIPE_IN     = 0x81
+        self.MAX_TRANSFER_SIZE = 1024
+        self.MAX_8BIT_DATA = 64
+
+
+    class Cmd():
+        """Enumeration of command codes."""
+        GET_VERSION         = 0xf1
+        DEBUG               = 0xf2
+        DFU                 = 0xf3
+        SWIM                = 0xf4
+        GET_CURRENT_MODE    = 0xf5
+        GET_TARGET_VOLTAGE  = 0xf7
+
+    class Mode():
+        """Enumeration of operation mode codes."""
+        (MODE_DFU,
+        MODE_MASS,
+        MODE_DEBUG,
+        MODE_SWIM,
+        MODE_BOOTLOADER) = range(0, 5)
+
+    class DFU():
+        """Enumeration of DFU codes."""
+        EXIT = 0x07
+
+    class SWIM():
+        """Enumeration of SWIM codes."""
+        ENTER = 0x00
+        EXIT  = 0x01
+
+    class Debug():
+        """Enumeration of Debug codes."""
+        ENTER_JTAG      = 0x00
+        STATUS          = 0x01
+        FORCEDEBUG      = 0x02
+        READMEM_32BIT   = 0x07
+        WRITEMEM_32BIT  = 0x08
+        RUNCORE         = 0x09
+        STEPCORE        = 0x0a
+        READMEM_8BIT    = 0x0c
+        WRITEMEM_8BIT   = 0x0d
+        EXIT            = 0x21
+        READCOREID      = 0x22
+        SYNC            = 0x3e
+        ENTER_SWD       = 0xa3
+
+        class A1():
+            """Enumeration of Debug A1 codes."""
+            RESETSYS         = 0x03
+            READALLREGS      = 0x04
+            READREG          = 0x05
+            WRITEREG         = 0x06
+            SETFP            = 0x0b
+            CLEARFP          = 0x0e
+            WRITEDEBUGREG    = 0x0f
+            SETWATCHPOINT    = 0x10
+            ENTER            = 0x20
+
+        class A2():
+            """Enumeration of Debug A2 codes."""
+            NRST_LOW         = 0x00
+            NRST_HIGH        = 0x01
+            NRST_PULSE       = 0x02
+            ENTER            = 0x30
+            READ_IDCODES     = 0x31
+            RESETSYS         = 0x32
+            READREG          = 0x33
+            WRITEREG         = 0x34
+            WRITEDEBUGREG    = 0x35
+            READDEBUGREG     = 0x36
+            READALLREGS      = 0x3a
+            GETLASTRWSTATUS  = 0x3b
+            DRIVE_NRST       = 0x3c
+            START_TRACE_RX   = 0x40
+            STOP_TRACE_RX    = 0x41
+            GET_TRACE_NB     = 0x42
+            SWD_SET_FREQ     = 0x43
+
+    class SWDFrequency():
+        """Enumeration of available SWD frequencies.
+
+        Default frequency is 1.8MHz
+        """
+        DEFAULT = 1,
+        MAP = {
+            4000000: 0,
+            1800000: 1,
+            1200000: 2,
+            950000:  3,
+            480000:  7,
+            240000: 15,
+            125000: 31,
+            100000: 40,
+            50000:  79,
+            25000: 158,
+            # 15000: 265,
+            # 5000:  798
+        }
 
 
 class STLinkV21(Device):
